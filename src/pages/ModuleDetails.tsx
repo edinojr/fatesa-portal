32c8990cfc87f1f0dd378c56e4984de380a61a3c
Edino_Junior <edi.ben.jr@gmail.com>
2026-06-10 10:50:41 -0300

    chore: update module details imports

--- src/pages/ModuleDetails.tsx
+++ src/pages/ModuleDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import {
   ChevronRight,
